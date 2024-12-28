6b3fe639397bdc1b6cac2c8f81d905305e3ba8ad
jawakarsri <jawaharsri2003@gmail.com>
2024-12-28 09:48:04 +0530

    Add solution for LeetCode problem 689 - Implement algorithm to find maximum sum of 3 non-overlapping subarrays

---
+++ 689.maximum-sum-of-3-non-overlapping-subarrays.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=689 lang=java
+ *
+ * [689] Maximum Sum of 3 Non-Overlapping Subarrays
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] result = new int[3];
+
+        int maxSum = sum[k] - sum[0];
+        for (int i = k; i < n; i++) {
+            if (sum[i + 1] - sum[i + 1 - k] > maxSum) {
+                left[i] = i + 1 - k;
+                maxSum = sum[i + 1] - sum[i + 1 - k];
+            } else {
+                left[i] = left[i - 1];
+            }
+        }
+
+        maxSum = sum[n] - sum[n - k];
+        right[n - k] = n - k;
+        for (int i = n - k - 1; i >= 0; i--) {
+            if (sum[i + k] - sum[i] >= maxSum) {
+                right[i] = i;
+                maxSum = sum[i + k] - sum[i];
+            } else {
+                right[i] = right[i + 1];
+            }
+        }
+
+        maxSum = 0;
+        for (int i = k; i <= n - 2 * k; i++) {
+            int l = left[i - 1];
+            int r = right[i + k];
+            int total = (sum[i + k] - sum[i]) + (sum[l + k] - sum[l]) + (sum[r + k] - sum[r]);
+            if (total > maxSum) {
+                maxSum = total;
+                result[0] = l;
+                result[1] = i;
+                result[2] = r;
+            }
+        }
+
+        return result;
+    }
+}
+// @lc code=end
+
