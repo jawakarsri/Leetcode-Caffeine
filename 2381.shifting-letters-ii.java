02379bcdcdb309e4008e6ad1042eb0e293862b77
jawakarsri <jawaharsri2003@gmail.com>
2025-01-05 16:12:35 +0530

    Add solution for LeetCode problem 2381 - Implement algorithm to shift letters based on given shifts

---
+++ 2381.shifting-letters-ii.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=2381 lang=java
+ *
+ * [2381] Shifting Letters II
+ */
+
+// @lc code=start
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length();
+        int[] shiftAmount = new int[n + 1];
+        
+        for (int[] shift : shifts) {
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2] == 1 ? 1 : -1;
+            
+            shiftAmount[start] += direction;
+            shiftAmount[end + 1] -= direction;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            shiftAmount[i] += shiftAmount[i - 1];
+        }
+        
+        char[] result = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            int shift = shiftAmount[i] % 26;
+            result[i] = (char) ((result[i] - 'a' + shift + 26) % 26 + 'a');
+        }
+        
+        return new String(result);
+    }
+}
+// @lc code=end
+
